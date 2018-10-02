a942b2232ccfe3c3ba331f32157f1f02a27f073b
Kosuke Tanaka <ktanaka@ktanaka.osaka.redhat.com>
2018-10-02 19:27:30 +0859

    modified

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
   echo "<h1>Hello, world!</h1>"; 
-  echo gethostname() . "\n";
-  echo $_SERVER['SERVER_ADDR']; ;?>
+  echo "hostname: " . gethostname() . "\n";
+  echo $_SERVER['SERVER_ADDR'] . "\n"; ;?>
