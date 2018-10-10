13a805b8641915003286c24a06aa053284c0e5e8
kostanaka <kosuke.tanaka@gmail.com>
2018-10-10 08:30:11 +0000

    updated.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
   echo "<h1>Hello, world!</h1>\n"; 
-  echo "HOSTNAME: " . gethostname() . "<BR>\n";
+  echo "Hostname: " . gethostname() . "<BR>\n";
   echo "IP-addr : " . $_SERVER['SERVER_ADDR'] . "\n"; ;?>
