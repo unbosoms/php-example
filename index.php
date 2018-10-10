8285330bc14db21779edd1e20051d7fb82b75fbe
kostanaka <kosuke.tanaka@gmail.com>
2018-10-10 09:21:43 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
-  echo "<h1>Hello, cruel world!</h1>\n"; 
-  echo "Hostname: " . gethostname() . "<BR>\n";
+  echo "<h1>Hello world!</h1>\n"; 
+  echo "hostname: " . gethostname() . "<BR>\n";
   echo "IP-addr : " . $_SERVER['SERVER_ADDR'] . "\n"; ;?>
