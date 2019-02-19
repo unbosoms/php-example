1c87fa86075917f83ad07dd572bf48fbf48dd5b8
unbosoms <34240826+unbosoms@users.noreply.github.com>
2019-02-19 10:14:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
-  echo "<h1>Hello Container World!</h1>\n"; 
+  echo "<h1>Good-bye Container World!</h1>\n"; 
   echo "hostname: " . gethostname() . "<BR>\n";
   echo "IP-Addr : " . $_SERVER['SERVER_ADDR'] . "\n"; ;?>
